4f16744e9886f7712f655ffe73eda4dd08c3c473
dportc01 <dportc01@estudiantes.unileon.es>
2024-12-28 15:21:47 +0100

    añadido algunas tablas

--- app/src/main/java/com/example/opengym/Model/OpenGymDbContract.java
+++ app/src/main/java/com/example/opengym/Model/OpenGymDbContract.java
@@ -3,14 +3,38 @@ package com.example.opengym.Model;
 public final class OpenGymDbContract {
     private OpenGymDbContract() {}
 
-    public static final String SQL_CREATE_ENTRIES =
+    public static final String SQL_CREATE_USERS_ENTRIES =
             "CREATE TABLE " + Users.TABLE_NAME + " (" +
                     Users.COLUMN_NAME + " TEXT PRIMARY KEY," +
                     Users.COLUMN_PASSWORD + " TEXT," +
-                    Users.COLUMN_PREMIUM + " INTEGER DEFAULT 0)";
+                    Users.COLUMN_PREMIUM + " INTEGER DEFAULT 0);";
+
+    public static final String SQL_CREATE_ROUTINES_ENTRIES =
+            "CREATE TABLE " + Routines.TABLE_NAME + " (" +
+                    Routines.COLUMN_NAME + " TEXT, " +
+                    Routines.COLUMN_DESCRIPTION + " TEXT, " +
+                    Routines.COLUMN_USERNAME + " TEXT, " +
+                    "PRIMARY KEY (" + Routines.COLUMN_USERNAME + ", " + Routines.COLUMN_NAME + "), " +
+                    "FOREIGN KEY (" + Routines.COLUMN_USERNAME + ") REFERENCES " + Users.TABLE_NAME + "(" + Users.COLUMN_NAME + "));";
+
+    public static final String SQL_CREATE_SESSIONS_ENTRIES =
+            "CREATE TABLE " + Sessions.TABLE_NAME + " (" +
+                    Sessions.COLUMN_NAME + " TEXT, " +
+                    Sessions.COLUMN_RESTDURATION + " INTEGER, " +
+                    Sessions.COLUMN_ROUTINENAME + " TEXT, " +
+                    Sessions.COLUMN_DATE + " TEXT, " +
+                    "PRIMARY KEY (" + Sessions.COLUMN_ROUTINENAME + ", " + Sessions.COLUMN_NAME + "), " +
+                    "FOREIGN KEY (" + Sessions.COLUMN_ROUTINENAME + ") REFERENCES " + Routines.TABLE_NAME + "(" + Routines.COLUMN_NAME + "));";
+
+    public static final String SQL_CREATE_ENTRIES =
+            SQL_CREATE_USERS_ENTRIES + " " +
+            SQL_CREATE_ROUTINES_ENTRIES + " " +
+            SQL_CREATE_SESSIONS_ENTRIES;
 
     public static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + Users.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + Users.TABLE_NAME + "; " +
+            "DROP TABLE IF EXISTS " + Routines.TABLE_NAME + "; " +
+            "DROP TABLE IF EXISTS " + Sessions.TABLE_NAME + ";";
 
     public static class Users {
 
